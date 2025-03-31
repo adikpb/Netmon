0dfaf112c8ff04b982c27188c9bf448c60570da0
adikpb <67222969+adikpb@users.noreply.github.com>
2025-03-31 23:02:38 +0530

    fix(color): Improve Color

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,21 @@
-def string_to_hex_color(s):
-    hex_value = int(s.encode("utf-8").hex(), 16)
-    r = hex(hex_value % 256)[2:].zfill(2)
-    g = hex((hex_value // 256) % 256)[2:].zfill(2)
-    b = hex((hex_value // 65536) % 256)[2:].zfill(2)
-    hex_color = "#{}{}{}".format(r.zfill(2), g.zfill(2), b.zfill(2))
-    return hex_color
+import colorsys
+import hashlib
 
 
-def lighten_hex_color(hex_color: str, amount: float):
+def lighten_hex_color_for_light_mode(
+    hex_color: str, min_lightness: float = 0.85
+) -> str:
     hex_color = hex_color.lstrip("#")
-    rgb = tuple(int(hex_color[i : i + 2], 16) for i in (0, 2, 4))
-    new_rgb = tuple(int(channel + (255 - channel) * amount) for channel in rgb)
-    new_hex_color = "#{:02x}{:02x}{:02x}".format(*new_rgb)
-    return new_hex_color
+    r, g, b = [int(hex_color[i : i + 2], 16) / 255.0 for i in (0, 2, 4)]
+
+    h, l, s = colorsys.rgb_to_hls(r, g, b)
+
+    l = max(l, min_lightness)
+
+    r, g, b = colorsys.hls_to_rgb(h, l, s)
+    return "#{:02x}{:02x}{:02x}".format(int(r * 255), int(g * 255), int(b * 255))
+
+
+def string_to_hex_color(s: str) -> str:
+    hash_digest = hashlib.md5(s.encode("utf-8")).hexdigest()
+    return f"#{hash_digest[:6]}"
