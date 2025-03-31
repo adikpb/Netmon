54053884f10ef9abef36cdff1f4137bd1c9bc578
adikpb <67222969+adikpb@users.noreply.github.com>
2025-03-31 23:41:58 +0530

    fix(async): Graceful Stopping

--- src/__main__.py
+++ src/__main__.py
@@ -1,17 +1,32 @@
+import logging
+import signal
 import sqlite3
-from capture import TrafficLogger
-from utils import lighten_hex_color, string_to_hex_color
+import sys
 
 import dash
 import pandas as pd
 import plotly.express as px
 from dash import Input, Output, dash_table, dcc, html
 
-# Initialize Dash app
+from capture import TrafficLogger
+from utils import lighten_hex_color_for_light_mode, string_to_hex_color
+
 app = dash.Dash(__name__)
+traffic_logger = TrafficLogger()
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
-# Fetch data from database
+def signal_handler(sig, frame):
+    logging.info("Gracefully shutting down...")
+    traffic_logger.stop_sniffer()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
 def fetch_data(protocol_filter=None):
     try:
         conn = sqlite3.connect("network_traffic.db")
@@ -27,33 +42,26 @@ def fetch_data(protocol_filter=None):
         conn.close()
         return df
     except Exception as e:
-        print(f"Error: {e}")
+        logging.error(f"Error: {e}")
+        return pd.DataFrame()
 
 
-# Get unique protocol types from database
 def get_protocol_types():
     try:
         conn = sqlite3.connect("network_traffic.db")
         cursor = conn.cursor()
         cursor.execute(
-            """
-            CREATE TABLE IF NOT EXISTS traffic (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                protocol TEXT, src_ip TEXT, dst_ip TEXT, src_port INTEGER, dst_port INTEGER,
-                packet_length INTEGER, timestamp TEXT
-            )
-        """
+            """CREATE TABLE IF NOT EXISTS traffic (id INTEGER PRIMARY KEY AUTOINCREMENT, protocol TEXT, src_ip TEXT, dst_ip TEXT, src_port INTEGER, dst_port INTEGER, packet_length INTEGER, timestamp TEXT)"""
         )
         cursor.execute("SELECT DISTINCT protocol FROM traffic")
         protocol_types = [row[0] for row in cursor.fetchall()]
         conn.close()
         return protocol_types
     except Exception as e:
-        print(f"Error: {e}")
+        logging.error(f"Error: {e}")
         return []
 
 
-# Layout for Dash App
 app.layout = html.Div(
     style={
         "background": "linear-gradient(to right, #e0f2f7, #b2ebf2)",
@@ -79,7 +87,7 @@ app.layout = html.Div(
         ),
         dcc.Interval(id="interval-component", interval=5000, n_intervals=0),
         dcc.Dropdown(
-            id="protocol-dropdown-filter",  # ID for the dropdown
+            id="protocol-dropdown-filter",
             options=[
                 {"label": "All", "value": "All"},
                 {"label": "Unknown", "value": "Unknown"},
@@ -95,7 +103,7 @@ app.layout = html.Div(
             id="traffic-table",
             columns=[
                 {"name": "Timestamp", "id": "timestamp"},
-                {"name": "Protocol", "id": "protocol"},  # remove filter_options
+                {"name": "Protocol", "id": "protocol"},
                 {"name": "Source IP", "id": "src_ip"},
                 {"name": "Destination IP", "id": "dst_ip"},
                 {"name": "Source Port", "id": "src_port"},
@@ -113,21 +121,14 @@ app.layout = html.Div(
                 "fontWeight": "600",
                 "borderBottom": "1px solid #dee2e6",
             },
-            style_cell={
-                "textAlign": "left",
-                "padding": "10px 15px",
-                "border": "none",
-            },
+            style_cell={"textAlign": "left", "padding": "10px 15px", "border": "none"},
             style_data_conditional=[
-                {
-                    "if": {"row_index": "odd"},
-                    "backgroundColor": "#f2f2f2",
-                }
+                {"if": {"row_index": "odd"}, "backgroundColor": "#f2f2f2"}
             ]
             + [
                 {
                     "if": {"filter_query": "{protocol} = " + f"'{protocol}'"},
-                    "backgroundColor": lighten_hex_color(
+                    "backgroundColor": lighten_hex_color_for_light_mode(
                         string_to_hex_color(protocol), 0.3
                     ),
                 }
@@ -186,23 +187,16 @@ def update_protocols(data, protocols, style):
         conn = sqlite3.connect("network_traffic.db")
         cursor = conn.cursor()
         cursor.execute(
-            """
-            CREATE TABLE IF NOT EXISTS traffic (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                protocol TEXT, src_ip TEXT, dst_ip TEXT, src_port INTEGER, dst_port INTEGER,
-                packet_length INTEGER, timestamp TEXT
-            )
-        """
+            """CREATE TABLE IF NOT EXISTS traffic (id INTEGER PRIMARY KEY AUTOINCREMENT, protocol TEXT, src_ip TEXT, dst_ip TEXT, src_port INTEGER, dst_port INTEGER, packet_length INTEGER, timestamp TEXT)"""
         )
         cursor.execute("SELECT DISTINCT protocol FROM traffic")
         protocol_types = set([row[0] for row in cursor.fetchall()])
         conn.close()
         currently_distinct_protocols = set(i["label"] for i in protocols)
-
         return style + [
             {
                 "if": {"filter_query": "{protocol} = " + f"'{protocol}'"},
-                "backgroundColor": lighten_hex_color(
+                "backgroundColor": lighten_hex_color_for_light_mode(
                     string_to_hex_color(protocol), 0.3
                 ),
             }
@@ -211,10 +205,9 @@ def update_protocols(data, protocols, style):
             {"label": protocol, "value": protocol}
             for protocol in protocol_types - currently_distinct_protocols
         ]
-
     except Exception as e:
-        print(f"Error: {e}")
-        return []
+        logging.error(f"Error: {e}")
+        return [], []
 
 
 @app.callback(
@@ -234,22 +227,18 @@ def update_dashboard(n, protocol_filter):
     try:
         df = fetch_data(protocol_filter)
         table_data = df.to_dict("records") if df is not None else None
-
         pie_chart = px.pie(df, names="protocol", title="Protocol Distribution")
         bar_chart = px.bar(
             df, x="protocol", y="packet_length", title="Packet Length by Protocol"
         )
-
         pie_chart.update_layout(
             plot_bgcolor="rgba(0,0,0,0)", paper_bgcolor="rgba(0,0,0,0)"
         )
         bar_chart.update_layout(
             plot_bgcolor="rgba(0,0,0,0)", paper_bgcolor="rgba(0,0,0,0)"
         )
-
         capture_duration = traffic_logger.get_capture_duration()
         capture_count = traffic_logger.get_capture_count()
-
         return (
             table_data,
             pie_chart,
@@ -258,7 +247,8 @@ def update_dashboard(n, protocol_filter):
             f"Total Captures: {capture_count}",
         )
     except Exception as e:
-        print(f"Error: {e}")
+        logging.error(f"Error: {e}")
+        return None, {}, {}, "", ""
 
 
 @app.callback(
@@ -273,11 +263,10 @@ def export_csv(n_clicks):
             df.to_csv if df is not None else None, "traffic_data.csv"
         )
     except Exception as e:
-        print(f"Error: {e}")
+        logging.error(f"Error: {e}")
+        return None
 
 
 if __name__ == "__main__":
-    traffic_logger = TrafficLogger()
     traffic_logger.start_sniffer()
     app.run(debug=True, use_reloader=True)
-    input()
